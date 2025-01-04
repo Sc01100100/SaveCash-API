f72c5348fa35bff8621e024f20ec9ed31012f2a4
nekowawolf <irukainu20@gmail.com>
2025-01-05 02:14:01 +0700

    refactor: removing unused dotenv

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"log"
-	"os"
+    "log"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-	"github.com/Sc01100100/SaveCash-API/config"
-	"github.com/Sc01100100/SaveCash-API/routes"
+    "github.com/gofiber/fiber/v2"
+    "github.com/Sc01100100/SaveCash-API/config"
+    "github.com/Sc01100100/SaveCash-API/routes"
 )
 
 func main() {
-    if os.Getenv("HEROKU") != "true" {
-        err := godotenv.Load()
-        if err != nil {
-            log.Fatalf("Error loading .env file: %v", err)
-        }
-    }
-
     config.ConnectDB()
     defer config.Database.Close()
 
@@ -29,3 +20,35 @@ func main() {
 
     log.Fatal(app.Listen(":8080"))
 }
+
+
+
+
+
+// package main
+
+// import (
+// 	"log"
+// 	"github.com/joho/godotenv"
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/Sc01100100/SaveCash-API/config"
+// 	"github.com/Sc01100100/SaveCash-API/routes"
+// )
+
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file: %v", err)
+// 	}
+
+// 	config.ConnectDB()
+// 	defer config.Database.Close()
+
+// 	log.Println("Database connection established.")
+
+// 	app := fiber.New()
+
+// 	routes.SetupRoutes(app)
+
+// 	log.Fatal(app.Listen(":8080"))
+// }
